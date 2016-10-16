a70ee7002e7f9bafae261938a0e668406a762a68
Manikandan81480 <mani81480@gmail.com>
2016-10-16 02:21:03 -0700

    Create CheckChar.java

---
+++ CheckChar.java
@@ -0,0 +1,9 @@
+class CheckChar
+{
+public static void main(string... args)
+{
+Scanner x=new Scanner(System.in);
+String s=x.nextLine();
+System.out.println("no of string in the string"+s.length());
+}
+}
